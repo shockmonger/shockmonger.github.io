f229b2c0e78aed4c25185ff983dc3b522e86454c
shockmonger <tejaswinee.k@research.iiit.ac.in>
2016-12-13 15:47:28 +0100

    message

--- create.php
+++ create.php
@@ -1,15 +1,9 @@
 <?php
-if(isset($_POST['message']) && isset($_POST['sender'])) {
-    $data = $_POST['message'] . '-' . $_POST['sender'] . "\n";
-    $ret = file_put_contents('data.txt', $data, FILE_APPEND | LOCK_EX);
-    if($ret === false) {
-        die('There was an error writing this file');
-    }
-    else {
-        echo "$ret bytes written to file";
-    }
-}
-else {
-   die('no post data to process');
+$txt = "data.txt"; 
+if (isset($_POST['message']) && isset($_POST['sender'])) { // check if both fields are set
+    $fh = fopen($txt, 'a'); 
+    $txt=$_POST['message'].' - '.$_POST['sender']; 
+    fwrite($fh,$txt); // Write information to the file
+    fclose($fh); // Close the file
 }
 ?>
